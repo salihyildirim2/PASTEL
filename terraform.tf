ce58f01a86553edbb29b3dedc78c96781596c87d
salihyildirim2 <67365057+salihyildirim2@users.noreply.github.com>
2020-07-12 13:35:37 +0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,2 +1,10 @@
 plugin_cache_dir   = "$HOME/.terraform.d/plugin-cache"
 disable_checkpoint = true
+~/.terraform.d/plugins
+ %APPDATA%\terraform.d\plugins
+~/.terraform.d/plugins/<OS>_<ARCH>
+  %APPDATA%\terraform.d\plugins\<OS>_<ARCH>
+  credentials_helper "example" {
+  args = [9]
+}
+complete
